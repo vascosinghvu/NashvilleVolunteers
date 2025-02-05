04b4b0eddeff30a5b92dbf08bbdfef7d0cf29266
charlotteconze <85513920+charlotteconze@users.noreply.github.com>
2025-02-05 14:22:50 -0600

    changes

--- app/src/components/Navbar.tsx
+++ app/src/components/Navbar.tsx
@@ -17,58 +17,27 @@ const Navbar = (): ReactElement => {
   }
 
   return (
-    <>
-      <div className="Navbar">
-        <div className="Navbar-body">
+    <div className="Navbar">
+      <div className="Navbar-body">
+        {/* Logo */}
+        <div
+          className="Navbar-body-logo"
+          onClick={() => navigate("/")}
+          style={{ cursor: "pointer" }}
+        >
+          <Icon glyph="heart" regular />
+          <span className="Text--bold">Nashville Volunteers</span>
+        </div>
+
+        {/* Navigation Links */}
+        <div className="Flex-row Margin-left--auto Align-items--center">
           <div
-            className="Navbar-body-logo"
+            className="Navbar-body-link Margin-right--20"
             onClick={() => navigate("/")}
-            style={{ cursor: "pointer" }}
           >
-            <Icon glyph="heart" regular />
-            <span className="Text--bold">Nashville Volunteers</span>
+            Home
           </div>
-          <div className="Flex-row Margin-left--auto Align-items--center">
-            <div
-              className="Navbar-body-link Margin-right--20"
-              onClick={() => {
-                navigate("/")
-              }}
-            >
-              Home
-            </div>
-            {user ? (
-              <>
-                <div
-                  className="Navbar-body-link Margin-right--20"
-                  onClick={() => navigate("/profile")}
-                >
-                  Profile
-                </div>
-                <div
-                  className="Button Button--small Button-color--blue-1000"
-                  onClick={handleSignOut}
-                >
-                  Logout
-                </div>
-              </>
-            ) : (
-              <>
-                <div
-                  className="Button Button-color--blue-1000 Button--small Margin-right--20"
-                  onClick={() => navigate("/login")}
-                >
-                  Login
-                </div>
-                <div
-                  className="Button Button-color--yellow-1000 Button--small"
-                  onClick={() => navigate("/signup")}
-                >
-                  Sign Up
-                </div>
-              </>
-            )}
-          </div>
+
           {user ? (
             <>
               <div
@@ -78,22 +47,22 @@ const Navbar = (): ReactElement => {
                 Profile
               </div>
               <div
-                className="Navbar-body-link"
+                className="Button Button--small Button-color--blue-1000"
                 onClick={handleSignOut}
               >
-                Sign Out
+                Logout
               </div>
             </>
           ) : (
             <>
               <div
-                className="Navbar-body-link Margin-right--20"
+                className="Button Button-color--blue-1000 Button--small Margin-right--20"
                 onClick={() => navigate("/login")}
               >
                 Login
               </div>
               <div
-                className="Navbar-body-link"
+                className="Button Button-color--yellow-1000 Button--small"
                 onClick={() => navigate("/signup")}
               >
                 Sign Up
