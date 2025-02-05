1f2c7e77b1f6bdbbaa0def820cdb51562f88e119
charlotteconze <85513920+charlotteconze@users.noreply.github.com>
2025-02-05 11:47:07 -0600

    initial progress

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,6 +2,7 @@ import React from "react"
 import { Route, Routes } from "react-router-dom"
 import Listings from "./pages/Listings"
 import Landing from "./pages/Landing"
+import Register from "./pages/Register"
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/listings" element={<Listings />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </div>
   )
---
+++ app/src/pages/Register.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Register = () => {
+  return (
+    <div className="Register">
+      <div className="Block">
+        <div className="Block-header">Register</div>
+      </div>
+    </div>
+  )
+}
+
+export default Register
