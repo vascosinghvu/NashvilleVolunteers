dad2e1c48ddba89a6bd0238145fd2c052db0c757
Vasco Singh <vasco@realignrx.com>
2025-02-11 20:55:45 -0600

    Added image support to 'Events'

--- app/src/components/Event.tsx
+++ app/src/components/Event.tsx
@@ -11,6 +11,7 @@ interface EventProps {
     location?: string
     people_needed?: number
     o_id: number
+    image_url?: string
   }
   organizationName?: string
   onClick?: () => void
@@ -19,13 +20,19 @@ interface EventProps {
 const Event: React.FC<EventProps> = ({ event, organizationName, onClick }) => {
   return (
     <div className="Event" onClick={onClick}>
-      <div className="Event-color">
-        {event.tags?.map((tag) => (
-          <div key={tag} className="Badge Badge-color--light-500">
-            {tag}
-          </div>
-        ))}
-      </div>
+      {event.image_url ? (
+        <div className="Event-image">
+          <img src={event.image_url} alt={event.name} />
+        </div>
+      ) : (
+        <div className="Event-color">
+          {event.tags?.map((tag) => (
+            <div key={tag} className="Badge Badge-color--light-500">
+              {tag}
+            </div>
+          ))}
+        </div>
+      )}
       <div className="Event-text">
         <div className="Event-text-title">{event.name}</div>
         {organizationName && <div className="Event-org">{organizationName}</div>}
